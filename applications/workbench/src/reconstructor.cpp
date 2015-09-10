ed99bec9198c57612e362d722af3d92775d33868
Gregory Kramida <algomorph@gmail.com>
2015-09-10 17:40:58 -0400

    Initial work on reconstructor threading.

--- applications/workbench/src/reconstructor.cpp
+++ applications/workbench/src/reconstructor.cpp
@@ -11,14 +11,46 @@
 namespace reco {
 namespace workbench {
 
-reconstructor::reconstructor(){
-	// TODO Auto-generated constructor stub
+reconstructor::reconstructor():
+	paused(false){
 
 }
 
 reconstructor::~reconstructor(){
 	// TODO Auto-generated destructor stub
 }
+
+void reconstructor::process_data(){
+	while(!stopped){
+		{
+		std::unique_lock<std::mutex> lck(pause_mutex);
+		//wait if paused
+		pause_cv.wait(lck, []{return !paused;});
+		}
+	}
+}
+
+void reconstructor::run(){
+	if(paused){
+		std::unique_lock<std::mutex> lck(pause_mutex);
+		paused = false;
+		pause_cv.notify_one();
+	}else{
+		this->thread = std::thread(&reconstructor::process_data, this);
+	}
+}
+
+void reconstructor::pause(){
+	std::unique_lock<std::mutex> lck(pause_mutex);
+	paused = true;
+}
+
+void reconstructor::stop(){
+	paused = false;
+	stopped = true;
+	this->thread.join();
+}
+
 
 } /* namespace workbench */
 } /* namespace reco */
--- applications/workbench/src/reconstructor.h
+++ applications/workbench/src/reconstructor.h
@@ -10,13 +10,38 @@
 #ifndef RECO_WORKBENCH_RECONSTRUCTOR_H_
 #define RECO_WORKBENCH_RECONSTRUCTOR_H_
 
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+
 namespace reco {
 namespace workbench {
 /**
  * Responsible for processing the range images and coming up with 3D meshes reconstructed from them
  */
 class reconstructor {
+private:
+
+	//locking
+	std::mutex pause_mutex;
+	std::thread thread;
+	std::condition_variable pause_cv;
+
+	//thread state variables
+	bool paused;
+	bool stopped;
+
+	//data processing
+	void process_data();
 public:
+
+
+	//thread run management
+	void run();
+	void pause();
+	void stop();
+
 	reconstructor();
 	virtual ~reconstructor();
 };
