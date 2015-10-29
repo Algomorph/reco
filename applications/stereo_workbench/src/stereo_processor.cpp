20d198a48ace73435c66228762893728bcbbbb4b
Gregory Kramida <algomorph@gmail.com>
2015-10-29 15:53:27 -0400

    empty matrix error fixed

--- applications/stereo_workbench/src/stereo_processor.cpp
+++ applications/stereo_workbench/src/stereo_processor.cpp
@@ -25,15 +25,14 @@
 namespace reco {
 namespace stereo_workbench {
 
-
 stereo_processor::stereo_processor(
 		datapipe::frame_buffer_type input_frame_buffer,
 		datapipe::frame_buffer_type output_frame_buffer,
 		std::shared_ptr<matcher_qt_wrapper_base> matcher,
-		std::shared_ptr<rectifier>  rectifier):
+		std::shared_ptr<rectifier> rectifier) :
 		worker(),
 
-				rectification_enabled((bool)rectifier),
+		rectification_enabled((bool) rectifier),
 				input_frame_buffer(input_frame_buffer),
 				output_frame_buffer(output_frame_buffer),
 				matcher(matcher),
@@ -41,26 +40,26 @@ stereo_processor::stereo_processor(
 				right_v_offset(0),
 				_rectifier(rectifier)
 {
-	connect(matcher.get(),SIGNAL(parameters_changed()), this, SLOT(recompute_disparity()));
+	connect(matcher.get(), SIGNAL(parameters_changed()), this, SLOT(recompute_disparity()));
 }
 
 stereo_processor::~stereo_processor() {
 
 }
 
-
 void stereo_processor::set_rectifier(std::shared_ptr<rectifier> _rectifier) {
 	std::unique_lock<std::mutex> lck(this->input_guard);
 	this->_rectifier = _rectifier;
-	if(!_rectifier){
+	if (!_rectifier) {
 		rectification_enabled = false;
 	}
-	if (!last_left.empty() &&rectification_enabled) {
-		//protect _rectifier from write access during rectification
-		_rectifier->rectify(last_left,last_right,last_left_rectified,last_right_rectified);
-		compute_disparity(last_left_rectified,last_right_rectified);
-	}else{
-		compute_disparity(last_left,last_right);
+	if (!last_left.empty()) {
+		if (rectification_enabled) {
+			_rectifier->rectify(last_left, last_right, last_left_rectified, last_right_rectified);
+			compute_disparity(last_left_rectified, last_right_rectified);
+		} else {
+			compute_disparity(last_left, last_right);
+		}
 	}
 }
 
@@ -73,7 +72,7 @@ void stereo_processor::pre_thread_join() {
 	input_frame_buffer->push_back(dummy);
 }
 
-std::shared_ptr<matcher_qt_wrapper_base> stereo_processor::get_matcher() const{
+std::shared_ptr<matcher_qt_wrapper_base> stereo_processor::get_matcher() const {
 	return matcher;
 }
 
@@ -84,7 +83,6 @@ bool stereo_processor::do_unit_of_work() {
 		cv::Mat left = *(array->at(0));
 		cv::Mat right = *(array->at(1));
 
-
 		std::unique_lock<std::mutex> lck(this->input_guard);
 		//TODO try without copying
 		left.copyTo(last_left);
@@ -92,10 +90,10 @@ bool stereo_processor::do_unit_of_work() {
 
 		if (rectification_enabled) {
 			//protect _rectifier from write access during rectification
-			_rectifier->rectify(last_left,last_right,last_left_rectified,last_right_rectified);
-			compute_disparity(last_left_rectified,last_right_rectified);
-		}else{
-			compute_disparity(last_left,last_right);
+			_rectifier->rectify(last_left, last_right, last_left_rectified, last_right_rectified);
+			compute_disparity(last_left_rectified, last_right_rectified);
+		} else {
+			compute_disparity(last_left, last_right);
 		}
 
 		return true;
@@ -106,28 +104,28 @@ bool stereo_processor::do_unit_of_work() {
 
 void stereo_processor::save_current_matcher_input() {
 	std::unique_lock<std::mutex> lck(this->input_guard);
-	if(rectification_enabled){
+	if (rectification_enabled) {
 		cv::imwrite("left.png", last_left_rectified);
 		cv::imwrite("right.png", last_right_rectified);
-	}else{
+	} else {
 		cv::imwrite("left.png", last_left);
 		cv::imwrite("right.png", last_right);
 	}
-	if(!disparity_normalized.empty()){
+	if (!disparity_normalized.empty()) {
 		cv::imwrite("output.png", disparity_normalized);
 	}
 }
 
-void stereo_processor::recompute_disparity(){
+void stereo_processor::recompute_disparity() {
 	std::unique_lock<std::mutex> lck(this->input_guard);
-	if(rectification_enabled){
-		compute_disparity(last_left_rectified,last_right_rectified);
-	}else{
-		compute_disparity(last_left,last_right);
+	if (rectification_enabled) {
+		compute_disparity(last_left_rectified, last_right_rectified);
+	} else {
+		compute_disparity(last_left, last_right);
 	}
 }
 
-bool stereo_processor::is_rectification_enabled() const{
+bool stereo_processor::is_rectification_enabled() const {
 	return this->rectification_enabled;
 }
 
@@ -154,7 +152,6 @@ void stereo_processor::compute_disparity(cv::Mat left, cv::Mat right) {
 
 	matcher->compute(left, right, disparity);
 
-
 	cv::normalize(disparity, disparity_normalized, 0, 255, cv::NORM_MINMAX, CV_8U);
 #ifdef MORPHOLOGY_CLOSE
 	cv::Mat kernel = cv::getStructuringElement(cv::MORPH_CROSS,cv::Size(3,3));
@@ -167,32 +164,29 @@ void stereo_processor::compute_disparity(cv::Mat left, cv::Mat right) {
 	emit frame(images);
 }
 
-
-int stereo_processor::get_v_offset() const{
+int stereo_processor::get_v_offset() const {
 	return this->right_v_offset;
 }
-
 
 void stereo_processor::set_v_offset(int value) {
 	right_v_offset = value;
 	recompute_disparity();
 }
 
-
-void stereo_processor::toggle_rectification(){
-	if(this->_rectifier){
+void stereo_processor::toggle_rectification() {
+	if (this->_rectifier) {
 		rectification_enabled = !rectification_enabled;
 		std::unique_lock<std::mutex> lck(this->input_guard);
-		if (!last_left.empty() && rectification_enabled) {
-			//protect _rectifier from write access during rectification
-			_rectifier->rectify(last_left,last_right,last_left_rectified,last_right_rectified);
-			compute_disparity(last_left_rectified,last_right_rectified);
-		}else{
-			compute_disparity(last_left,last_right);
+		if (!last_left.empty()) {
+			if (rectification_enabled) {
+				_rectifier->rectify(last_left, last_right, last_left_rectified, last_right_rectified);
+				compute_disparity(last_left_rectified, last_right_rectified);
+			} else {
+				compute_disparity(last_left, last_right);
+			}
 		}
 	}
 }
-
 
 } /* namespace stereo_workbench */
 } /* namespace reco */
